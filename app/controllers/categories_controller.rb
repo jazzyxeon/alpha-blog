8f0b08f70f4486cff3c4edc932a0cfc342876ce2
kmugi <mugi.mugiwarapirates@gmail.com>
2017-04-22 12:38:36 +0000

    Added edit and delete features to index listing of categories for admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :update, :show, :destroy]
   before_action :require_admin, except: [:index, :show]
   
   def index
@@ -20,11 +21,10 @@ class CategoriesController < ApplicationController
   end
   
   def edit
-    @category = Category.find(params[:id])
+    
   end
   
   def update
-    @category = Category.find(params[:id])
     if @category.update(category_params)
       flash[:success] = "Category name was successfully updated"
       redirect_to category_path(@category)
@@ -34,11 +34,20 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
+  def destroy
+    @category.destroy
+    flash[:danger] = "Category was successfully deleted"
+    redirect_to categories_path
+  end
+  
   private
+    def set_category
+      @category = Category.find(params[:id])
+    end
+    
     def category_params
       params.require(:category).permit(:name)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  resources :categories, except: [:destroy]
+  resources :categories
   # You can have the root of your site routed with "root"
   #root 'welcome#index'
 
